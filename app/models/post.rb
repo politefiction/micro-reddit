fbb3b988fd896e44021c1ec35eff65ef9f23bdd8
Chelsi Jones <cajones.bz@gmail.com>
2018-01-29 09:55:55 -0600

    Add Comments table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
 	validates :title, presence: true, length: { maximum: 100 }
 	validates :content, presence: true, length: { maximum: 2000 }
+
+	belongs_to :user
+	has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	before_save { name.downcase }
 	validates :name, presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false, message: "someone already beat you to it" }
-	#has_many_posts
-	#has_many_comments
+
+	has_many :posts, dependent: :destroy
+	has_many :comments, dependent: :destroy
 end
